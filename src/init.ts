3a6bfce52c310f82d605d62a605ef0b6eb247afa
ErrolKeith <errol.k.keith@gmail.com>
2024-12-22 17:32:16 -0500

    fix comment

--- src/init.ts
+++ src/init.ts
@@ -2,7 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 
 /**
- * `initializeUi` should is the applications main entry point.
+ * `initializeUi` is the applications main entry point.
  * @param uiOptions
  */
 export function initializeUi(uiOptions: { mountId: string }) {
