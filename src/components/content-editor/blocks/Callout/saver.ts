e670a50f5518e9cedded1a8d8c6bdf45a06ffdee
ErrolKeith <errol.k.keith@gmail.com>
2025-01-07 13:44:07 -0500

    save callout

--- src/components/content-editor/blocks/Callout/saver.ts
+++ src/components/content-editor/blocks/Callout/saver.ts
@@ -4,7 +4,8 @@ import type { ToolSaver } from "../utils/editor-block-builder";
 export const calloutToolSaver: ToolSaver<CalloutData> = (
   blockContent: HTMLElement
 ) => {
+  const callout = blockContent.querySelector(".callout-text");
   return {
-    calloutText: "",
+    calloutText: callout?.innerHTML ?? undefined,
   };
 };
